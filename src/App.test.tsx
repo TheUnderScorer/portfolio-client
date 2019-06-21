8c73b96a7ba6c89c6605aee98803840b8a5dfcac
Przemysław Żydek <przemyslaw.z@mpcreation.net>
2019-06-21 09:55:06 +0200

    Updated tests, moved font awesome loader to App.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it( 'renders without crashing', () => {
-    const div = document.createElement( 'div' );
-    ReactDOM.render( <App/>, div );
-    ReactDOM.unmountComponentAtNode( div );
+describe( 'App component', () => {
+
+    let root: HTMLDivElement;
+
+    beforeEach( () => {
+        root = document.createElement( 'div' );
+        root.id = 'root';
+
+        document.body.appendChild( root );
+    } );
+
+    afterEach( () => {
+        root.remove();
+    } );
+
+    it( 'renders without crashing', () => {
+        ReactDOM.render( <App/>, root );
+        ReactDOM.unmountComponentAtNode( root );
+    } );
+
+
 } );
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import homeStore from './stores/homeStore';
 import Home from './pages/Home';
+import './fontAwesome';
 
 const App = () => {
     return (
