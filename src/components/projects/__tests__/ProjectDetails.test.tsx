f5e02b08f6a15055b0ab87de16ec8d2d26937b5c
Przemysław Żydek <przemyslawzydek@gmail.com>
2019-09-04 11:21:11 +0200

    Added project details tests

---
+++ src/components/projects/__tests__/ProjectDetails.test.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import projects from '../../../pages/data/projects';
+import { mountWithStore } from '../../../tests/utils/enzyme/renderer';
+import ProjectDetails from '../ProjectDetails';
+import '../../../fontAwesome';
+import { ProjectImage } from '../styled';
+import { act } from 'react-dom/test-utils';
+import Loader from '../../loader/Loader';
+import { wait } from '@apollo/react-testing';
+
+const project = projects[ 0 ];
+
+const initialState = {
+    theme: {
+        mode: 'white'
+    }
+};
+
+describe( 'ProjectDetails component', () =>
+{
+    let onImageLoad: jest.Mock;
+
+    beforeEach( () =>
+    {
+        onImageLoad = jest.fn();
+    } );
+
+    it( 'Renders without crashing', () =>
+    {
+        mountWithStore(
+            <ProjectDetails project={ project }/>,
+            initialState
+        );
+    } );
+
+    it( 'Triggers onImageLoad whenever image loads and hides loader', async () =>
+    {
+        const { component } = mountWithStore(
+            <ProjectDetails onImageLoad={ onImageLoad } project={ project }/>,
+            initialState
+        );
+
+        const image = component.find( ProjectImage ).at( 0 );
+
+        act( () =>
+        {
+            image.simulate( 'load' );
+        } );
+
+        expect( onImageLoad ).toBeCalledTimes( 1 );
+        expect( onImageLoad ).toBeCalledWith( 0 );
+
+        await wait( 1500 );
+
+        const loader = component.update().find( Loader );
+        const loaderProps = loader.props();
+
+        expect( loaderProps.active ).toBeFalsy();
+    } );
+
+    it( 'Displays loader when images have not loaded yet', () =>
+    {
+        const { component } = mountWithStore(
+            <ProjectDetails project={ project }/>,
+            initialState
+        );
+
+        const loader = component.find( Loader );
+        const loaderProps = loader.props();
+
+        expect( loader ).toHaveLength( 1 );
+        expect( loaderProps.active ).toBeTruthy();
+    } );
+} );
